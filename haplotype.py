e344ef54522d61e3112a2543a51311bba0974f84
dangertrip <dangertrip@hotmail.com>
2020-03-25 10:13:08 -0500

    Regions will be printed out

--- haplotype.py
+++ haplotype.py
@@ -45,6 +45,7 @@ def main():
     cpgfile = sys.argv[1]
     bamfile = sys.argv[2]
     fn=bamfile
+    cg_monitor = {}
     with open(cpgfile) as f:
         lines = f.readlines()
     dic={}
@@ -55,6 +56,7 @@ def main():
             dic[chr].append(int(s))
         else:
             dic[chr]=[int(s)]
+        cg_monitor[chr+'_'+s] = [0,0]
     
     for chr in dic:
         dic[chr].sort()
@@ -103,24 +105,45 @@ def main():
             if num-s+1>=len(reads) or num-s-1<=1:
                 continue
             cpg_in_reads=reads[num-s-1:num-s+1]
+            mark = chr+'_'+str(num)
+            #print(cg_monitor[mark])
             if cpg_in_reads==strand_specific_table[0]:
+                if cg_monitor[mark][0]!=0 and cg_monitor[mark][0]!=cg_monitor[mark][1]:
+                    seq_err=True
+                    break
                 cpg_status=cpg_status+'C'
                 cpg_pos = cpg_pos+str(num)+','
+                cg_monitor[mark][0]+=1 
+                cg_monitor[mark][1]+=1
                 containc=1
             else:
                 if cpg_in_reads==strand_specific_table[1]:
+                    if cg_monitor[mark][0]!=0 and cg_monitor[mark][0]!=cg_monitor[mark][1]:
+                        seq_err=True
+                        break
                     cpg_status=cpg_status+'T'
                     cpg_pos = cpg_pos+str(num)+','
+                    cg_monitor[mark][0]+=0
+                    cg_monitor[mark][1]+=1
                     containt=1
                     #Position should be different in +/- strand for cpg. I will deal with this later.
                 else:
+                    cpg_status=cpg_status+'N'
+                    cpg_pos = cpg_pos+str(num)+','
                     #print(cpg_in_reads,strand_specific_table,num,s)
-                    seq_err=True
-                    break
+                    #seq_err=True
+                    #break
         #print(cpg_status,seq_err)
         #print(temp,flag)
+        #print(seq_err)
+        if not cpg_status: continue
         if seq_err:
+            num = str(num)
+            while num in result[-1]:
+                result.pop()
             continue
+        result.append(str(chr)+'\t'+str(s)+'\t'+str(e-1)+'\t'+str(name)+'\t'+str(cpg_status)+'\t'+str(strand)+'\t'+str(cpg_pos[:-1])+'\t'+str(flag)+'\t'+str(mismatch)+'\n')
+        '''
         if cpg_status:
             dup_s=chr+str(s)+str(e-1)+cpg_status+strand+cpg_pos[:-1]
             if dup_s in dup_dic:
@@ -133,10 +156,11 @@ def main():
                     lastreads=lastreads[1:]
                     dup_dic.remove(remove_s)
             result.append(str(chr)+'\t'+str(s)+'\t'+str(e-1)+'\t'+str(name)+'\t'+str(cpg_status)+'\t'+str(strand)+'\t'+str(cpg_pos[:-1])+'\t'+str(flag)+'\t'+str(mismatch)+'\n')
-            if len(result)>100000:
-                with open(fn+'.status','a') as ff:
-                    ff.writelines(result)
-                result=[]
+        '''
+        if len(result)>1000000:
+            with open(fn+'.status','a') as ff:
+                ff.writelines(result)
+            result=[]
     f.close()
     with open(fn+'.status','a') as ff:
         ff.writelines(result)
